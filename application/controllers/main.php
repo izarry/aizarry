741ff9bd7302b748ac7e8293451f201ea6263ae7
izarry <larryriomawile@gmail.com>
2014-09-19 15:26:39 +0800

    2nd day

---
+++ application/controllers/main.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('header');
+		$this->load->view('body');
+		$this->load->view('footer');
+	}
+}
